756e2c90abc34a7eaf21de8d74b89bc23d1d6a53
Mozaaar <141757001+Mozaaar@users.noreply.github.com>
2024-11-16 15:15:37 -0500

    Allowed_hosts

--- envios/settings.py
+++ envios/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2m)8*-i+ew3e16eb#e6i24$37(f)**g0ckm0a(&ffe-fg=jmc6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mozaaar.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
